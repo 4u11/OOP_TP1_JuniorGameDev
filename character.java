cbaa986f6eb17a2b77e9caba7b3b536ef23fb0ca
4u11 <mifmaul06@gmail.com>
2025-03-23 06:35:22 +0700

    First Commit

---
+++ character.java
@@ -0,0 +1,15 @@
+public class character {
+    private String name; //Serana, the daughter of ColdHarbour Family
+    private int HP; //Health Points
+    private int XP; //Experience Points
+    private int MP; //Mana Points
+    private String items;
+
+    public character(String name, int health, int XP, int Mana) {
+        this.name = name;
+        this.HP = HP;
+        this.XP = 0;
+        this.MP = MP;
+        this.items = "";
+    }
+}
